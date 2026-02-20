9ca5b7694b071b7229af9e9f548bbece51342bab
Minn Maung Maung <miniminnmgmg24@gmail.com>
2026-02-20 15:34:35 +0630

    Quartz sync: Feb 20, 2026, 3:34 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "☕ miniMinn.md",
+    pageTitle: "☕ miniMinn",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
