25e93008e227f044128877b837d7f6cef060f392
Minn Maung Maung <miniminnmgmg24@gmail.com>
2026-05-18 23:45:06 +0630

    Quartz sync: May 18, 2026, 11:45 PM

--- quartz/components/Profile.tsx
+++ quartz/components/Profile.tsx
@@ -2,7 +2,7 @@ import { QuartzComponent, QuartzComponentConstructor } from "./types"
 
 const Profile: QuartzComponent = () => {
   return (
-    <a href="/miniMinn.md" class="profile-link-wrapper">
+    <a href="/miniMinn.md/" class="profile-link-wrapper">
       <div class="profile-card">
         <img
           src="https://media.licdn.com/dms/image/v2/D5635AQGz7-ZwQ1qt8w/profile-framedphoto-shrink_800_800/B56Z4WN7RpIQAg-/0/1778489210308?e=1779717600&v=beta&t=wwanKgX0JVL36T-bFyEaIfvhnMDMI3T4LNlq34aMank"
